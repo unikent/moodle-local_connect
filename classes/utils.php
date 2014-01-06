5a2f8eb51b928454c4619892b1ea4304fc6bcaba
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-01-06 14:56:21 +0000

    Add new utils class

---
+++ classes/utils.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local stuff for Moodle Connect
+ *
+ * @package    core_connect
+ * @copyright  2014 Skylar Kelty <S.Kelty@kent.ac.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_connect;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Connect utils
+ */
+class utils {
+
+	/**
+	 * Is connect configured properly?
+	 */
+	public static function is_enabled() {
+		global $CFG;
+		return $CFG->kent->environment === "dev" || ($CFG->kent->environment === "live" && $CFG->kent->distribution === LIVE_MOODLE);
+	}
+
+}
--- proxy.php
+++ proxy.php
@@ -6,7 +6,7 @@ require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
 require_once(dirname(__FILE__) . '/locallib.php');
 require_once(dirname(__FILE__) . '/proxylib.php');
 
-if (!($CFG->kent->environment === "dev" || ($CFG->kent->environment === "live" && $CFG->kent->distribution === LIVE_MOODLE))) {
+if (!\local_connect\utils::is_enabled()) {
   die(json_encode(array("error" => "Connect has been disabled")));
 }
 
