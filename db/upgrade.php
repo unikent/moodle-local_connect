f0fe66bcf5f92b56ee048c476a6e3474c8633af2
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-01-09 13:03:39 +0000

    Add table for tracking CHKSUM changes

--- db/upgrade.php
+++ db/upgrade.php
@@ -71,5 +71,31 @@ function xmldb_local_connect_upgrade($oldversion) {
 		upgrade_plugin_savepoint(true, 2012052913, 'local', 'connect');
 	}
 
+    if ($oldversion < 2014010901) {
+
+        // Define table course_chksum to be created.
+        $table = new xmldb_table('course_chksum');
+
+        // Adding fields to table course_chksum.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('module_delivery_key', XMLDB_TYPE_CHAR, '36', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('session_code', XMLDB_TYPE_CHAR, '4', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('chksum', XMLDB_TYPE_CHAR, '36', null, null, null, null);
+
+        // Adding keys to table course_chksum.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->add_key('course_unique', XMLDB_KEY_UNIQUE, array('courseid'));
+        $table->add_key('sdsuid_unique', XMLDB_KEY_UNIQUE, array('module_delivery_key', 'session_code'));
+
+        // Conditionally launch create table for course_chksum.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Connect savepoint reached.
+        upgrade_plugin_savepoint(true, 2014010901, 'local', 'connect');
+    }
+
     return true;
 }
