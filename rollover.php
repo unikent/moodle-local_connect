1e3cd860fcf6065a659c1312a48a97e1450419eb
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-01-14 13:57:08 +0000

    Improve UX of rollover Beta page

--- rollover.php
+++ rollover.php
@@ -4,6 +4,7 @@
  */
 
 require(dirname(__FILE__) . '/../../config.php');
+require_once($CFG->dirroot.'/local/connect/rollover_form.php');
 
 global $PAGE, $OUTPUT, $USER, $OUTPUT;
 
@@ -11,6 +12,7 @@ $PAGE->set_context(context_system::instance());
 $PAGE->set_url('/local/connect/rollover.php');
 $title = get_string('rollover_heading', 'local_connect');
 $PAGE->set_title($title);
+$PAGE->set_pagelayout('admin');
 
 if (!\local_connect\utils::enable_new_features()) {
 	print_error('new_feature_disabled', 'local_connect');
@@ -25,33 +27,13 @@ $PAGE->requires->js_init_call('M.local_rollover.init', array(), false, array(
 echo $OUTPUT->header();
 echo $OUTPUT->heading($title);
 
-echo '<div class="warning box">Warning! These pages are currently in BETA and may not be stable.</div>';
-
-echo $OUTPUT->box_start();
-echo '<form action="rollover.php" method="post" id="rolloversettings">';
-echo '<div class="settingsform clearfix">';
-echo html_writer::input_hidden_params($PAGE->url);
-echo '<input type="hidden" name="sesskey" value="'.sesskey().'" />';
-
-echo '<div class="c2 fld sourceid"><label for="rollover_source">Rollover source: </label>';
-echo '<select name="rollover_source" id="rollover_source">';
-echo '<option value="release">Moodle RD</option>';
-echo '<option value="2013">Moodle 2013</option>';
-echo '<option value="2012">Moodle 2012</option>';
-echo '<option value="archive">Moodle Archive</option>';
-echo '</select>';
-echo '</div>';
-
-echo '<div class="c1 fld targetid"><label for="rollover_target">Rollover into: </label>';
-echo '<input type="text" name="target_id" id="rollover_target" /></div>';
-
-echo '<div class="c2 fld sourceid"><label for="source_id">Rollover from: </label>';
-echo '<input type="text" name="source_id" id="source_id" /></div>';
-
-echo '<div class="form-buttons"><input class="form-submit" type="submit" value="Rollover" /></div>';
-
-echo '</div>';
-echo '</form>';
+echo $OUTPUT->box_start('noticebox', 'notice');
+echo html_writer::tag('b', 'Warning! These pages are currently in BETA and may not be stable.');
 echo $OUTPUT->box_end();
+
+$rolloverform = new connect_rollover_form(null, array());
+if ($rolloverdata = $rolloverform->get_data()) {
+}
+$rolloverform->display();
 
 echo $OUTPUT->footer();
--- scripts/rollover.js
+++ scripts/rollover.js
@@ -5,16 +5,16 @@ M.local_rollover = {
     Y : null,
     transaction : [],
     init : function(Y) {
-        Y.one('#rollover_target').plug(Y.Plugin.AutoComplete, {
+        Y.one('#id_target').plug(Y.Plugin.AutoComplete, {
           source: M.cfg.wwwroot + '/local/connect/ajax/course_search.php?name={query}'
         });
 
-        Y.one('#source_id').plug(Y.Plugin.AutoComplete, {
-          source: M.cfg.wwwroot + '/local/connect/ajax/course_search.php?name={query}&source=' + Y.one('#rollover_source').get("value")
+        Y.one('#id_source').plug(Y.Plugin.AutoComplete, {
+          source: M.cfg.wwwroot + '/local/connect/ajax/course_search.php?name={query}&source=' + Y.one('#id_source_moodle').get("value")
         });
 
-        Y.one('#rollover_source').on('change', function(e) {
-            Y.one('#source_id').ac.set('source', M.cfg.wwwroot + '/local/connect/ajax/course_search.php?name={query}&source=' + e.target.get("value"));
+        Y.one('#id_source_moodle').on('change', function(e) {
+            Y.one('#id_source').ac.set('source', M.cfg.wwwroot + '/local/connect/ajax/course_search.php?name={query}&source=' + e.target.get("value"));
         });
     }
 }
