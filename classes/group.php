705d1840e6fc653ed8b9a45c2d64409194090d13
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-01-22 15:45:21 +0000

    Add in code for group creation

--- classes/group.php
+++ classes/group.php
@@ -40,11 +40,97 @@ class group {
     /** Our description */
     public $description;
 
-    /** Our module delivery key */
+    /** Our Connect course */
+    public $course;
+
+    /** Our course's module delivery key */
     public $module_delivery_key;
 
-    /** Our session code */
+    /** Our course's session code */
     public $session_code;
+
+    /**
+     * Grab our Connect Course
+     */
+    private function get_course() {
+    	if (isset($this->course)) {
+    		return $this->course;
+    	}
+
+    	$this->course = course::get_course_by_uid($this->module_delivery_key, $this->session_code);
+
+    	return $this->course;
+    }
+
+    /**
+     * Grab (or create) our grouping ID
+     */
+    private function get_or_create_grouping() {
+    	global $DB;
+
+		$grouping = $DB->get_record('groupings', array(
+			'name' => 'Seminar groups',
+			'courseid' => $this->moodle_id
+		));
+
+		// Create?
+		if (!$grouping) {
+	    	$data = new \stdClass();
+			$data->name = "Seminar groups";
+			$data->courseid = $course->moodle_id;
+			$data->description = '';
+			return groups_create_grouping($data);
+		}
+
+		return $grouping->id;
+    }
+
+    /**
+     * Check to see if this exists in Moodle
+     */
+    public function is_in_moodle() {
+    	global $DB;
+
+    	$course = $this->get_course();
+    	$course_moodle_id = $course->moodle_id;
+
+        $sql = "SELECT COUNT(g.id) FROM {groups} g WHERE g.courseid=:courseid AND g.name=:name";
+        $params = array(
+        	"courseid" => $course_moodle_id,
+        	"name" => $this->name
+        );
+
+        return $DB->count_records_sql($sql, $params) > 0;
+    }
+
+    /**
+     * Create this group in Moodle
+     */
+    public function create_in_moodle() {
+    	$course = $this->get_course();
+
+    	if (empty($course->moodle_id)) {
+    		return false;
+    	}
+
+    	$data = new \stdClass();
+    	$data->name = $this->description;
+		$data->courseid = $course->moodle_id;
+		$data->description = '';
+
+    	$this->moodle_id = groups_create_group($data);
+
+		if ($this->moodle_id === false) {
+			throw new Exception("Could not create group '{$data->name}'!");
+		}
+
+		$grouping_id = $this->get_or_create_grouping();
+
+		// And add this group to the grouping
+		groups_assign_grouping($grouping_id, $this->moodle_id);
+
+		return true;
+    }
 
     /**
      * Returns all known groups for a given course.
@@ -68,9 +154,10 @@ class group {
         	$obj->id = $group->id;
         	$obj->moodle_id = $group->moodle_id;
         	$obj->description = $group->description;
+        	$obj->course = $course;
         	$obj->module_delivery_key = $course->module_delivery_key;
         	$obj->session_code = $course->session_code;
-        	
+
         	$group = $obj;
         }
 
