13ef3e4129b9c6c4406e1e7579c2af5fac2fd027
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-04-15 15:35:05 +0100

    Add group_uid reference in PHP

--- classes/group.php
+++ classes/group.php
@@ -43,14 +43,14 @@ class group extends data
      * A list of valid fields for this data object.
      */
     protected final static function valid_fields() {
-        return array("id", "mid", "courseid", "name");
+        return array("id", "mid", "courseid", "name", "group_uid");
     }
 
     /**
      * A list of immutable fields for this data object.
      */
     protected static function immutable_fields() {
-        return array("id");
+        return array("id", "group_uid");
     }
 
     /**
