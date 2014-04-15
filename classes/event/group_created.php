f59572ef114331a2478597d02702155f9c24ceee
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-04-15 09:37:33 +0100

    Require courseid

--- classes/event/group_created.php
+++ classes/event/group_created.php
@@ -65,6 +65,20 @@ class group_created extends \core\event\base
 	 * @return array
 	 */
 	protected function get_legacy_logdata() {
-		return array($this->objectid, 'connect', 'add', 'group.php?id=' . $this->objectid);
+		return array($this->courseid, 'connect', 'add', 'group.php?id=' . $this->objectid);
+	}
+
+	/**
+	 * Custom validation.
+	 *
+	 * @throws \coding_exception
+	 * @return void
+	 */
+	protected function validate_data() {
+		parent::validate_data();
+
+		if (!isset($this->courseid)) {
+			throw new \coding_exception('The \'courseid\' must be set.');
+		}
 	}
 }
