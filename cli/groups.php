d5078d95b300062aa4f2e3c15e815a4528fd01b0
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-04-17 14:02:11 +0100

    Standards

--- cli/groups.php
+++ cli/groups.php
@@ -13,85 +13,93 @@ $res = array();
 /*
  * expects input : (needs user fields in here too)
  * [
- *  { 'username' => 'abc', 'group_desc' => 'something', 'moodle_course_id' => 321
- *    'isa' => 'NEW' or 'DELETE'
- *    'moodle_group_id' => 354 or nil }
- *  ...
+ *    { 'username' => 'abc', 'group_desc' => 'something', 'moodle_course_id' => 321
+ *        'isa' => 'NEW' or 'DELETE'
+ *        'moodle_group_id' => 354 or nil }
+ *    ...
  * ]
  *
  * sends output :
  * [
- *   { 'result' => 'ok' or 'error', 'exception' => if error
- *     'moodle_group_id' => 345, 'moodle_user_id' => 123, 'in' => input }
+ *     { 'result' => 'ok' or 'error', 'exception' => if error
+ *         'moodle_group_id' => 345, 'moodle_user_id' => 123, 'in' => input }
  * ]
  */
 
-foreach( json_decode(file_get_contents('php://stdin')) as $c ) {
-  global $DB;
-  $tr = array();
-  $group = (object) array();
-  try {
-    $uid = !empty($c->username) ? $DB->get_record('user',array('username'=>$c->username)) : false;
+foreach (json_decode(file_get_contents('php://stdin')) as $c) {
+    global $DB;
+    $tr = array();
+    $group = (object) array();
 
-    if(!$uid) throw new moodle_exception('user not created yet?');
+    try {
+        $uid = !empty($c->username) ? $DB->get_record('user', array('username' => $c->username)) : false;
 
-    if($c->isa == 'NEW') {
-      $uid = $uid->id;
+        if (!$uid) {
+          throw new moodle_exception('user not created yet?');
+        }
 
-      $grouping = null;
-      if(empty($c->moodle_group_id)) {
-        $data = (object) array( 'name' => $c->group_desc, 'courseid' => $c->moodle_course_id );
-        $group = groups_create_group($data);
-        $c->moodle_group_id = $group;
-        if(! ($grouping = $DB->get_record('groupings',array('name'=>'Seminar groups','courseid'=>$c->moodle_course_id))) ) {
-          $data->name = "Seminar groups";
-          $grouping = groups_create_grouping($data);
+        if ($c->isa == 'NEW') {
+            $uid = $uid->id;
+
+            $grouping = null;
+            if (empty($c->moodle_group_id)) {
+                $data = (object) array( 'name' => $c->group_desc, 'courseid' => $c->moodle_course_id );
+                $group = groups_create_group($data);
+                $c->moodle_group_id = $group;
+                if (!($grouping = $DB->get_record('groupings', array('name' => 'Seminar groups', 'courseid' => $c->moodle_course_id)))) {
+                    $data->name = "Seminar groups";
+                    $grouping = groups_create_grouping($data);
+                } else {
+                    $grouping = $grouping->id;
+                }
+
+                if (!($g = $DB->get_record('groupings_groups',array('groupid' => $group, 'groupingid' => $grouping)))) {
+                    groups_assign_grouping($grouping, $group);
+                }
+            }
+
+            $group = $DB->get_record('groups', array('id' => $c->moodle_group_id));
+
+            if ($group === false) {
+                throw new moodle_exception('Group does not exist');
+            }
+
+            $r = groups_add_member($group,$uid);
+            if (!$r) {
+                $reason = '';
+                if (!is_enrolled(context_course::instance($c->moodle_course_id), $uid)) {
+                    $reason = 'user isnt enrolled on module '.$group->courseid;
+                }
+
+                throw new moodle_exception('group_add_member failed for '.$group->id.' and '.$uid.' '.$reason);
+            }
+        } else if ($c->isa == 'DELETE') {
+            if ($uid) {
+                // If the user doesnt exist, their enrolments should have been wiped already
+                // for now just remove the user, not the group
+                $group = $DB->get_record('groups', array('id' => $c->moodle_group_id));
+                if ($group) {
+                    // cant find the group, must already not be there
+                    $r = groups_remove_member($group,$uid);
+                    if (!$r) {
+                        throw new moodle_exception('group_remove_member failed for '.$group->id.' and '.$uid);
+                    }
+                }
+            }
         } else {
-          $grouping = $grouping->id;
+            throw new moodle_exception('dont understand ' . $c->isa);
         }
-        if(!($g = $DB->get_record('groupings_groups',array('groupid'=>$group,'groupingid'=>$grouping))) ) {
-          groups_assign_grouping($grouping, $group);
-        }
-      }
 
-      $group = $DB->get_record('groups',array('id'=>$c->moodle_group_id));
-
-      if ($group === false) {
-        throw new moodle_exception('Group does not exist');
-      }
-
-      $r = groups_add_member($group,$uid);
-      if(!$r) {
-        $reason = '';
-        if( !is_enrolled(context_course::instance($c->moodle_course_id), $uid) ) {
-          $reason = 'user isnt enrolled on module '.$group->courseid;
-        }
-        throw new moodle_exception('group_add_member failed for '.$group->id.' and '.$uid.' '.$reason);
-      }
-
-    } else if($c->isa == 'DELETE') {
-      if($uid) { // if the user doesnt exist, their enrolments should have been wiped already
-        // for now just remove the user, not the group
-        $group = $DB->get_record('groups',array('id'=>$c->moodle_group_id));
-        if($group) { // cant find the group, must already not be there
-          $r = groups_remove_member($group,$uid);
-          if(!$r) {
-            throw new moodle_exception('group_remove_member failed for '.$group->id.' and '.$uid);
-          }
-        }
-      }
-    } else {
-      throw new moodle_exception('dont understand ' . $c->isa);
+        $tr = array( 'result' => 'ok', 'moodle_group_id' => $group->id, 'moodle_user_id' => $uid, 'in' => $c );
+    } catch (Exception $e) {
+        $tr = array(
+            'result' => 'error',
+            'in' => $c,
+            'exception' => $e
+        );
     }
 
-    $tr = array( 'result' => 'ok', 'moodle_group_id' => $group->id, 'moodle_user_id' => $uid, 'in' => $c );
-  } catch( Exception $e ) {
-    $tr = array(
-      'result' => 'error',
-      'in' => $c,
-      'exception' => $e );
-  }
-  $res []= $tr;
+    $res[] = $tr;
 }
 
 echo json_encode($res);
