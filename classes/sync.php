4a2020a50bcaa81292e63958fbb1662d190a7d7f
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-04-22 15:52:31 +0100

    Add method for grabbing a list of enrolment IDs that need to be deleted

---
+++ classes/sync.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local stuff for Moodle Connect
+ *
+ * @package    local_connect
+ * @copyright  2014 Skylar Kelty <S.Kelty@kent.ac.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_connect;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Connect Sync Scripts
+ */
+class sync
+{
+    /**
+     * Map a dataset to an id array
+     */
+    private static function map_set($data) {
+        $ids = array();
+
+        foreach ($data as $datum) {
+            $ids[] = $datum->id;
+        }
+
+        return $ids;
+    }
+
+    /**
+     * Grab a list of enrolments due to be deleted (that have not yet been deleted)
+     */
+    public static function get_deleted_enrolments() {
+        global $DB;
+
+        $data = $DB->get_records_sql("SELECT ce.id
+                                        FROM {connect_enrolments} ce
+                                            INNER JOIN {connect_user} cu ON ce.userid=cu.id
+                                            INNER JOIN {connect_course} cc ON ce.courseid=cc.id
+                                            INNER JOIN (
+                                                SELECT ue.userid, e.courseid
+                                                FROM {user_enrolments} ue
+                                                INNER JOIN {enrol} e ON e.id=ue.enrolid
+                                            ) tt ON tt.userid=cu.mid AND tt.courseid=cc.mid
+                                        WHERE ce.deleted=1");
+
+        return self::map_set($data);
+    }
+}
---
+++ tests/sync_test.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Tests new Kent sync code
+ */
+class kent_sync_tests extends local_connect\util\connect_testcase
+{
+	/**
+	 * Make sure we can grab a valid list of enrolments to be deleted.
+	 */
+	public function test_enrolment_deletes() {
+		global $CFG, $DB;
+
+		$this->resetAfterTest();
+
+		$course = $this->generate_course();
+		$course_obj = \local_connect\course::get($course);
+		$course_obj->create_in_moodle();
+
+		$this->assertEquals(array(), \local_connect\sync::get_deleted_enrolments());
+
+		// Create some fake enrolments.
+		$this->generate_enrolments(30, $course, 'student');
+		$this->generate_enrolments(2, $course, 'convenor');
+		$teacher = $this->generate_enrolment(1, $course, 'teacher');
+		$teacher = $DB->get_record('connect_enrolments', array("id" => $teacher));
+
+		$enrolments = \local_connect\enrolment::get_all();
+		foreach ($enrolments as $enrolment) {
+			$enrolment->create_in_moodle();
+		}
+
+		$this->assertEquals(array(), \local_connect\sync::get_deleted_enrolments());
+
+		// Now mark one for deletion.
+		$teacher->deleted = 1;
+		$DB->update_record('connect_enrolments', $teacher);
+
+		$this->assertEquals(array($teacher->id), \local_connect\sync::get_deleted_enrolments());
+	}
+}
