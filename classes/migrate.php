0009bdd5362a116619121a51285c2d1bc077bdd4
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-04-22 13:12:27 +0100

    Fix up stuff

--- classes/migrate.php
+++ classes/migrate.php
@@ -89,18 +89,20 @@ class migrate
 	 * New Roles
 	 */
 	public static function new_roles() {
-		global $DB;
+		global $DB, $CFG;
+
+		$connect_db = $CFG->connect->db["name"];
 
 		echo "Migrating new roles\n";
 
 		$sql = "REPLACE INTO {connect_role} (name) (
 			SELECT e.role
-			FROM `connect_2013`.`enrollments` e
+			FROM `$connect_db`.`enrollments` e
 			LEFT OUTER JOIN {connect_role} cr
 				ON cr.name=e.role
 			WHERE cr.id IS NULL
 			GROUP BY e.role
-		);";
+		)";
 
 		return $DB->execute($sql);
 	}
@@ -109,18 +111,20 @@ class migrate
 	 * New Users
 	 */
 	public static function new_users() {
-		global $DB;
+		global $DB, $CFG;
+
+		$connect_db = $CFG->connect->db["name"];
 
 		echo "Migrating new users\n";
 
 		$sql = "REPLACE INTO {connect_user} (ukc, login, title, initials, family_name) (
 			SELECT e.ukc, e.login, e.title, e.initials, e.family_name
-			FROM `connect_2013`.`enrollments` e
+			FROM `$connect_db`.`enrollments` e
 			LEFT OUTER JOIN {connect_user} u
 				ON u.login=e.login
 			WHERE u.id IS NULL
 			GROUP BY e.login
-		);";
+		)";
 
 		return $DB->execute($sql);
 	}
@@ -129,18 +133,20 @@ class migrate
 	 * New Campuses
 	 */
 	public static function new_campus() {
-		global $DB;
+		global $DB, $CFG;
+
+		$connect_db = $CFG->connect->db["name"];
 
 		echo "Migrating new campus\n";
 
 		$sql = "REPLACE INTO {connect_campus} (`id`, `name`) (
 			SELECT c.campus, c.campus_desc
-			FROM `connect_2013`.`courses` c
+			FROM `$connect_db`.`courses` c
 			LEFT OUTER JOIN {connect_campus} cc
 				ON cc.id=c.campus
 			WHERE cc.id IS NULL
 			GROUP BY c.campus
-		);";
+		)";
 
 		return $DB->execute($sql);
 	}
@@ -149,7 +155,9 @@ class migrate
 	 * Updated Courses
 	 */
 	public static function updated_courses() {
-		global $DB;
+		global $DB, $CFG;
+
+		$connect_db = $CFG->connect->db["name"];
 
 		echo "Migrating updated courses\n";
 
@@ -159,7 +167,7 @@ class migrate
 			INNER JOIN {connect_course} cc ON cc.module_delivery_key=c.module_delivery_key AND cc.session_code=c.session_code
 			WHERE c.module_title <> cc.module_title OR c.module_code <> cc.module_code OR c.synopsis <> cc.synopsis OR c.category_id <> cc.category
 			GROUP BY c.module_delivery_key,c.session_code,c.module_version
-		);";
+		)";
 
 		return $DB->execute($sql);
 	}
@@ -168,17 +176,19 @@ class migrate
 	 * New Courses
 	 */
 	public static function new_courses() {
-		global $DB;
+		global $DB, $CFG;
+
+		$connect_db = $CFG->connect->db["name"];
 
 		echo "Migrating new courses\n";
 
 		$sql = "REPLACE INTO {connect_course} (module_delivery_key,session_code,module_version,campusid,module_week_beginning,module_length,week_beginning_date,module_title,module_code,synopsis,category, mid) (
 			SELECT c.module_delivery_key,c.session_code,COALESCE(c.module_version,1),c.campus as campusid,c.module_week_beginning,c.module_length,c.week_beginning_date,c.module_title,c.module_code,COALESCE(c.synopsis, ''),c.category_id,c.moodle_id as mid
-			FROM `connect_2013`.`courses` c
+			FROM `$connect_db`.`courses` c
 			LEFT OUTER JOIN {connect_course} cc ON cc.module_delivery_key=c.module_delivery_key AND cc.session_code=c.session_code
 			WHERE cc.id IS NULL
 			GROUP BY c.module_delivery_key,c.session_code,c.module_version
-		);";
+		)";
 
 		return $DB->execute($sql);
 	}
@@ -187,7 +197,9 @@ class migrate
 	 * Updated Groups
 	 */
 	public static function updated_groups() {
-		global $DB;
+		global $DB, $CFG;
+
+		$connect_db = $CFG->connect->db["name"];
 
 		echo "Migrating updated groups\n";
 
@@ -198,7 +210,7 @@ class migrate
 			INNER JOIN {connect_group} cg ON cg.id=g.group_id
 			WHERE g.group_desc <> cg.name
 			GROUP BY g.group_id
-		);";
+		)";
 
 		return $DB->execute($sql);
 	}
@@ -207,18 +219,20 @@ class migrate
 	 * New Groups
 	 */
 	public static function new_groups() {
-		global $DB;
+		global $DB, $CFG;
+
+		$connect_db = $CFG->connect->db["name"];
 
 		echo "Migrating new groups\n";
 
 		$sql = "REPLACE INTO {connect_group} (`id`, `courseid`, `name`, `mid`) (
 			SELECT g.group_id, c.id, g.group_desc, g.moodle_id
-			FROM `connect_2013`.`groups` g
+			FROM `$connect_db`.`groups` g
 			INNER JOIN {connect_course} c ON c.module_delivery_key=g.module_delivery_key AND c.session_code=g.session_code
 			LEFT OUTER JOIN {connect_group} cg ON cg.id=g.group_id
 			WHERE cg.id IS NULL
 			GROUP BY g.group_id
-		);";
+		)";
 
 		return $DB->execute($sql);
 	}
@@ -227,7 +241,9 @@ class migrate
 	 * Updated Enrolments
 	 */
 	public static function updated_enrolments() {
-		global $DB;
+		global $DB, $CFG;
+
+		$connect_db = $CFG->connect->db["name"];
 
 		echo "Migrating updated enrolments\n";
 
@@ -240,7 +256,7 @@ class migrate
 			INNER JOIN {connect_enrolments} ce ON ce.courseid=c.id AND ce.userid=u.id AND ce.roleid=r.id
 			WHERE e.sink_deleted <> ce.deleted
 			GROUP BY ce.id
-		);";
+		)";
 
 		return $DB->execute($sql);
 	}
@@ -249,19 +265,21 @@ class migrate
 	 * New Enrolments
 	 */
 	public static function new_enrolments() {
-		global $DB;
+		global $DB, $CFG;
+
+		$connect_db = $CFG->connect->db["name"];
 
 		echo "Migrating new enrolments\n";
 
 		$sql = "REPLACE INTO {connect_enrolments} (`courseid`, `userid`, `roleid`,`deleted`) (
 			SELECT c.id, u.id, r.id, e.sink_deleted
-			FROM `connect_2013`.`enrollments` e
+			FROM `$connect_db`.`enrollments` e
 			INNER JOIN {connect_course} c ON c.module_delivery_key=e.module_delivery_key AND c.session_code=e.session_code
 			INNER JOIN {connect_user} u ON u.login=e.login
 			INNER JOIN {connect_role} r ON r.name=e.role
 			LEFT OUTER JOIN {connect_enrolments} ce ON ce.courseid=c.id AND ce.userid=u.id AND ce.roleid=r.id
 			WHERE ce.id IS NULL
-		);";
+		)";
 
 		return $DB->execute($sql);
 	}
@@ -270,18 +288,20 @@ class migrate
 	 * Updated Group Enrolments
 	 */
 	public static function updated_group_enrolments() {
-		global $DB;
+		global $DB, $CFG;
+
+		$connect_db = $CFG->connect->db["name"];
 
 		echo "Migrating updated group enrolments\n";
 
 		$sql = "REPLACE INTO {connect_group_enrolments} (`id`, `groupid`, `userid`,`deleted`) (
 			SELECT cge.id, ge.group_id, u.id, ge.sink_deleted
-			FROM `connect_2013`.`group_enrollments` ge
+			FROM `$connect_db`.`group_enrollments` ge
 			INNER JOIN {connect_user} u ON u.login=ge.login
 			INNER JOIN {connect_group_enrolments} cge ON cge.groupid=ge.group_id AND cge.userid=u.id
 			WHERE cge.deleted <> ge.sink_deleted
 			GROUP BY cge.id
-		);";
+		)";
 
 		return $DB->execute($sql);
 	}
@@ -290,17 +310,19 @@ class migrate
 	 * New Group Enrolments
 	 */
 	public static function new_group_enrolments() {
-		global $DB;
+		global $DB, $CFG;
+
+		$connect_db = $CFG->connect->db["name"];
 
 		echo "Migrating new group enrolments\n";
 
 		$sql = "REPLACE INTO {connect_group_enrolments} (`groupid`, `userid`,`deleted`) (
 			SELECT ge.group_id, u.id, ge.sink_deleted
-			FROM `connect_2013`.`group_enrollments` ge
+			FROM `$connect_db`.`group_enrollments` ge
 			INNER JOIN {connect_user} u ON u.login=ge.login
 			LEFT OUTER JOIN {connect_group_enrolments} cge ON cge.groupid=ge.group_id AND cge.userid=u.id
 			WHERE cge.id IS NULL
-		);";
+		)";
 
 		return $DB->execute($sql);
 	}
@@ -309,6 +331,8 @@ class migrate
 	 * Map old roles to connect
 	 */
 	public static function map_roles($dry = false) {
+		global $DB;
+
 		$c_roles = $DB->get_records('connect_role', array("mid" => 0));
 		foreach ($c_roles as $c_role) {
 			$c_role_obj = role::get($c_role->id);
@@ -333,6 +357,8 @@ class migrate
 	 * Map old users to connect
 	 */
 	public static function map_users($dry = false) {
+		global $DB;
+
 		$c_users = $DB->get_records('connect_user', array("mid" => 0));
 		foreach ($c_users as $c_user) {
 			// Try to find a matching user.
