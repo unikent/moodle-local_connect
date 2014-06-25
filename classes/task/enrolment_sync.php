3afae886ddeb25bebf87c59ba66014035a52ae18
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-06-25 08:42:13 +0100

    Some of these should be doing that

--- classes/task/enrolment_sync.php
+++ classes/task/enrolment_sync.php
@@ -39,5 +39,7 @@ class enrolment_sync extends task_base
             $result = $obj->sync();
             $self->map_status($result, $obj);
         });
+
+        \core\task\manager::clear_static_caches();
     }
 } 
