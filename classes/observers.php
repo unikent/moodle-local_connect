de46fe53f6c27db09d2a4fb7369781f8b44b520b
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-06-25 08:41:23 +0100

    Move enrolment observer to task

--- classes/observers.php
+++ classes/observers.php
@@ -101,19 +101,11 @@ class observers
         $user = user::from_sql_result($user);
 
         // If we created the user on first login, sync enrolments.
-        // TODO - make this a "task" in 2.7.
-
-        // Sync Enrollments.
-        $enrolments = enrolment::get_by("userid", $user->id, true);
-        foreach ($enrolments as $enrolment) {
-            $enrolment->create_in_moodle();
-        }
-
-        // Sync Group Enrollments.
-        $enrolments = group_enrolment::get_by("userid", $user->id, true);
-        foreach ($enrolments as $enrolment) {
-            $enrolment->create_in_moodle();
-        }
+        $task = new \local_connect\task\user_enrolments();
+        $task->set_custom_data(array(
+            'userid' => $user->id
+        ));
+        \core\task\manager::queue_adhoc_task($task);
 
         return true;
     }
