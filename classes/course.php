1b0b9d362fa1de754c4bf6c5b64e99774951ccc5
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-07-11 09:14:17 +0100

    Port sync_enrolments to enrol plugin

--- classes/course.php
+++ classes/course.php
@@ -684,17 +684,16 @@ class course extends data
 
     /**
      * Syncs enrollments for this Course
-     * @todo Updates/Deletions
      */
     public function sync_enrolments() {
         if (!$this->is_in_moodle()) {
             return;
         }
 
-        foreach ($this->enrolments as $enrolment) {
-            if (!$enrolment->is_in_moodle()) {
-                $enrolment->create_in_moodle();
-            }
+        $instance = $this->get_enrol_instance();
+        if ($instance && $instance->status == ENROL_INSTANCE_ENABLED) {
+            $enrol = enrol_get_plugin('connect');
+            $enrol->sync($this->mid, array($instance));
         }
     }
 
