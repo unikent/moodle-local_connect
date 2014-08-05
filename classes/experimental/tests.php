28a7831e1a5e499dab4acc81be89a38524a2c8ce
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-08-05 11:56:47 +0100

    Destroy this

--- classes/experimental/tests.php
+++ classes/experimental/tests.php
@@ -2,5 +2,5 @@
 
 require_once('../../../../config.php');
 
-$objs = \local_connect\experimental\SDS\enrolments::get_all_teachers(2015);
-print_r(($objs));
+//$objs = \local_connect\experimental\SDS\enrolments::get_all_teachers(2015);
+//print_r(($objs));
