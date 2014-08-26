7b0afae6c4c686d31127b9474103dc286f521778
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-08-26 15:57:26 +0100

    Add group enrolment sync

--- classes/experimental/SDS/group_enrolments.php
+++ classes/experimental/SDS/group_enrolments.php
@@ -24,6 +24,9 @@
 
 namespace local_connect\experimental\SDS;
 
+global $CFG;
+require_once($CFG->libdir . '/ddllib.php');
+
 /**
  * Grabs all group enrolments out of SDS.
  */
@@ -31,19 +34,16 @@ class group_enrolments {
     /**
      * Grab out of SDS.
      */
-    public static function get_all() {
+    private function get_all() {
         global $CFG, $SDSDB;
 
         db::obtain();
 
         $sql = <<<SQL
             SELECT DISTINCT
-              ltrim(rtrim(dgm.session_code)) + '|' + ltrim(rtrim(cg.group_id)) + '|' + ltrim(rtrim(bd.email_address)) AS chksum,
+              ltrim(rtrim(cg.group_id)) + '|' + ltrim(rtrim(bd.email_address)) AS chksum,
               ltrim(rtrim(cg.group_id)) AS group_id,
-              ltrim(rtrim(cg.group_desc)) AS group_desc,
-              ltrim(rtrim(dgm.module_delivery_key)) AS module_delivery_key,
-              ltrim(rtrim(dgm.session_code)) AS session_code,
-              lug.ukc
+              ltrim(rtrim(bd.email_address)) as login
             FROM d_group_module AS dgm
               INNER JOIN c_groups AS cg ON cg.parent_group = dgm.group_id
               LEFT JOIN l_ukc_group AS lug on lug.group_id = cg.group_id
@@ -53,12 +53,87 @@ class group_enrolments {
               AND bd.email_address != ''
 SQL;
 
-        $objects = $SDSDB->get_records_sql($sql);
+        return $SDSDB->get_records_sql($sql);
+    }
 
-        foreach ($objects as $object) {
-            $object->chksum = md5($object->chksum);
-        }
+    /**
+     * Get a temptable for the sync.
+     */
+    private function get_temp_table() {
+        global $CFG;
 
-        return $objects;
+        require_once($CFG->libdir . '/ddllib.php');
+
+        $table = new \xmldb_table('tmp_connect_group_enrolments');
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '18', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('group_id', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('login', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        return $table;
+    }
+
+    /**
+     * Deleted Group Enrolments
+     */
+    private function sync_deleted_group_enrolments() {
+        global $DB;
+
+        echo "  - Migrating updated group enrolments\n";
+
+        return $DB->execute("
+            REPLACE INTO {connect_group_enrolments} (id, groupid, userid, deleted)
+            (
+                SELECT cge.id, cge.groupid, cge.id, 1
+                FROM {connect_group_enrolments} cge
+                INNER JOIN {connect_user} u ON u.id=cge.userid
+                LEFT OUTER JOIN {tmp_connect_group_enrolments} tcge
+                    ON tcge.group_id = cge.groupid AND tcge.login = u.login
+                WHERE tcge.id IS NULL
+            )
+        ");
+    }
+
+    /**
+     * New Group Enrolments
+     */
+    private function sync_new_group_enrolments() {
+        global $DB;
+
+        echo "  - Migrating new group enrolments\n";
+
+        return $DB->execute("
+            INSERT INTO {connect_group_enrolments} (groupid, userid, deleted)
+            (
+                SELECT ge.group_id, u.id, 0
+                FROM {tmp_connect_group_enrolments} ge
+                INNER JOIN {connect_user} u ON u.login = ge.login
+                LEFT OUTER JOIN {connect_group_enrolments} cge ON cge.groupid=ge.group_id AND cge.userid=u.id
+                WHERE cge.id IS NULL
+            )
+        ");
+    }
+
+    /**
+     * Sync groups with Moodle.
+     */
+    public function sync() {
+        global $CFG, $DB;
+
+        // Create a temp table.
+        $table = $this->get_temp_table();
+        $dbman = $DB->get_manager();
+        $dbman->create_temp_table($table);
+
+        // Load data into the temp table.
+        $DB->insert_records('tmp_connect_group_enrolments', $this->get_all());
+
+        // Move data over.
+        $this->sync_deleted_group_enrolments();
+        $this->sync_new_group_enrolments();
+
+        // Drop the temp table.
+        $dbman->drop_table($table);
     }
 }
